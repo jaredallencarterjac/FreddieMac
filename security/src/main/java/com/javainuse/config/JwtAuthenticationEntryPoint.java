f8a476aebb1d0891c0c55ba0e6d4783584732b2d
Jared Carter <jaredallencarterjac@gmail.com>
2020-05-06 08:57:54 -0700

    jwt

--- security/src/main/java/com/javainuse/config/JwtAuthenticationEntryPoint.java
+++ security/src/main/java/com/javainuse/config/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 
 package com.javainuse.config;
+
 import java.io.IOException;
 import java.io.Serializable;
 import javax.servlet.http.HttpServletRequest;
@@ -7,12 +8,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
-private static final long serialVersionUID = -7858869558953243875L;
-@Override
-public void commence(HttpServletRequest request, HttpServletResponse response,
-AuthenticationException authException) throws IOException {
-response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-}
+	private static final long serialVersionUID = -7858869558953243875L;
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+	}
 }
